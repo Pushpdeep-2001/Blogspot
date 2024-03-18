c8d1a924be3425c1c9a1dabe378a7819e9c0181b
Pushpdeep-2001 <pushpdeepsingh96@gmail.com>
2024-03-18 19:42:39 +0530

    last push

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
+const Blog = require("./models/blog");
+
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
+
+const app = express();
+const PORT = process.env.PORT || 8009;
+
+mongoose.connect(process.env.MONGO_URL)
+.then((e) => console.log('MongoDB connected'));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended:false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public/images")));
+
+app.get("/", async (req,res) => {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    });
+});
+
+app.use("/user",userRoute);
+app.use("/blog",blogRoute);
+
+
+app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
